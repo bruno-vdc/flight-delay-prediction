d8bbc70bd3298ab230425dc145be65d888bd1a05
bruno <brunovarizidocarmo@MacBook-Air-de-bruno.local>
2026-07-05 21:57:02 -0300

    Finished data processing

--- scripts/data_processing.py
+++ scripts/data_processing.py
@@ -23,7 +23,8 @@ df_flights_proc = df_flights.query('CANCELLED==0').reset_index(drop=True)
 
 #=========== dropping unnecessary columns and missing values ===========
 df_flights_proc = df_flights_proc.drop(columns=['CANCELLED', 'CANCELLATION_CODE', 'DELAY_DUE_CARRIER', 'DELAY_DUE_WEATHER',
-                                                'DELAY_DUE_NAS', 'DELAY_DUE_SECURITY', 'DELAY_DUE_LATE_AIRCRAFT'])
+                                                'DELAY_DUE_NAS', 'DELAY_DUE_SECURITY', 'DELAY_DUE_LATE_AIRCRAFT',
+                                                'AIRLINE_DOT', 'DOT_CODE'])
 
 miss_val_cols = ['ARR_DELAY', 'ELAPSED_TIME', 'AIR_TIME', 'TAXI_IN', 'WHEELS_ON', 'ARR_TIME', 'WHEELS_OFF', 'TAXI_OUT',
                  'DEP_DELAY', 'DEP_TIME', 'CRS_ELAPSED_TIME']
@@ -35,10 +36,13 @@ for col in miss_val_cols:
 # =========== transforming date and time columns ===========
 df_flights_proc['FL_DATE'] = pd.to_datetime(df_flights_proc['FL_DATE'], format='%Y-%m-%d')
 
-time_cols = ['CRS_DEP_TIME', 'DEP_TIME', 'CRS_ARR_TIME', 'ARR_TIME']
+time_cols = ['CRS_DEP_TIME', 'DEP_TIME', 'WHEELS_OFF', 'WHEELS_ON', 'CRS_ARR_TIME', 'ARR_TIME']
 
 for col in time_cols:
     df_flights_proc[col] = df_flights_proc[col].astype(int)
     df_flights_proc[col] = df_flights_proc[col].astype(str).str.zfill(4)
     df_flights_proc[col] = np.where(df_flights_proc[col]=='2400', '0000', df_flights_proc[col])
     df_flights_proc[col] = pd.to_datetime(df_flights_proc[col], format='%H%M')
+
+# =========== saving processed data ===========
+df_flights_proc.to_csv(DATA_DIR / 'flights_sample_proc.csv', index=False)
