8cd7682b8ddc7d2ce28847c8b0a021b5dc122436
bruno <brunovarizidocarmo@MacBook-Air-de-bruno.local>
2026-07-07 19:09:59 -0300

    Finish feature engineering

--- scripts/feature_engineering.py
+++ scripts/feature_engineering.py
@@ -3,6 +3,7 @@
 import pandas as pd
 import numpy as np
 from pathlib import Path
+import holidays
 
 #setting pandas options
 pd.set_option('display.max_columns', None) #columns
@@ -14,6 +15,11 @@ PROJECT_ROOT = Path(__file__).resolve().parent.parent
 DATA_DIR = PROJECT_ROOT / 'data'
 
 df_flights_proc = pd.read_csv(DATA_DIR / 'flights_sample_proc.csv')
+
+#=========== restoring data types ===========
+df_flights_proc['FL_DATE'] = pd.to_datetime(df_flights_proc['FL_DATE'], format='%Y-%m-%d')
+df_flights_proc["CRS_DEP_TIME"] = pd.to_datetime(df_flights_proc["CRS_DEP_TIME"])
+df_flights_proc["CRS_ARR_TIME"] = pd.to_datetime(df_flights_proc["CRS_ARR_TIME"])
 
 # =========== lists and dictionaries ===========
 #U.S.A. states and territories division
@@ -51,29 +57,55 @@ list_non_contiguous = list_caribbean + ['AK', 'HI']
 #distance categories
 #kilometers per mile converter
 kpm_conv = 1.609344
+bins = [0, 500, 1500, 4000, float('inf')]
+labels = ['Very Short Haul', 'Short Haul', 'Medium Haul', 'Long Haul']
 
-flight_distance = [(df_flights_proc['DISTANCE']*kpm_conv < 500),
-                   (df_flights_proc['DISTANCE']*kpm_conv >= 500) & (df_flights_proc['DISTANCE']*kpm_conv < 1500),
-                   (df_flights_proc['DISTANCE']*kpm_conv >= 1500) & (df_flights_proc['DISTANCE']*kpm_conv < 4000),
-                   (df_flights_proc['DISTANCE']*kpm_conv > 4000)]
+#federal holidays
+usa_holidays = holidays.US()
 
-distance_class = ['Very Short Haul', 'Short Haul', 'Medium Haul', 'Long Haul']
-
-# %%
 # =========== features ===========
+flights_featured = df_flights_proc.copy()
+
 #creating the target variable
-df_flights_proc['is_delayed'] = np.where(df_flights_proc['ARR_DELAY']>=15, 1, 0)
+flights_featured['is_delayed'] = np.where(flights_featured['ARR_DELAY']>=15, 1, 0)
 
 #geographical features
-df_flights_proc['origin_state'] = df_flights_proc['ORIGIN_CITY'].str[-2:]
-df_flights_proc['origin_division'] = df_flights_proc['origin_state'].map(division_map)
-df_flights_proc['origin_contiguous'] = np.where(df_flights_proc['origin_state'].isin(list_non_contiguous), 1, 0)
+flights_featured['origin_state'] = flights_featured['ORIGIN_CITY'].str[-2:]
+flights_featured['origin_division'] = flights_featured['origin_state'].map(division_map)
+flights_featured['origin_contiguous'] = np.where(flights_featured['origin_state'].isin(list_non_contiguous), 1, 0)
 
-df_flights_proc['dest_state'] = df_flights_proc['DEST_CITY'].str[-2:]
-df_flights_proc['dest_division'] = df_flights_proc['dest_state'].map(division_map)
-df_flights_proc['dest_contiguous'] = np.where(df_flights_proc['dest_state'].isin(list_non_contiguous), 1, 0)
+flights_featured['dest_state'] = flights_featured['DEST_CITY'].str[-2:]
+flights_featured['dest_division'] = flights_featured['dest_state'].map(division_map)
+flights_featured['dest_contiguous'] = np.where(flights_featured['dest_state'].isin(list_non_contiguous), 1, 0)
 
 #distance categories feature
-df_flights_proc['distance_category'] = np.select(flight_distance, distance_class)
+flights_featured['distance_category'] = pd.cut(flights_featured['DISTANCE']*kpm_conv, bins=bins, labels=labels, right=False)
 
-#time and date features
+#time features
+flights_featured["crs_departure_minutes"] = ((flights_featured["CRS_DEP_TIME"].dt.hour*60)+flights_featured["CRS_DEP_TIME"].dt.minute)
+flights_featured["crs_arrival_minutes"] = ((flights_featured["CRS_ARR_TIME"].dt.hour*60)+flights_featured["CRS_ARR_TIME"].dt.minute)
+
+#date features
+flights_featured["year"] = pd.to_datetime(flights_featured["FL_DATE"]).dt.year
+flights_featured["month"] = pd.to_datetime(flights_featured["FL_DATE"]).dt.month
+flights_featured["day_of_week"] = pd.to_datetime(flights_featured["FL_DATE"]).dt.day_name()
+
+#holiday flag
+flights_featured["is_holiday"] = (flights_featured["FL_DATE"].dt.date.isin(usa_holidays).astype(int))
+
+#seasonal periods
+periods = [(flights_featured["month"].isin([12, 1, 2])),
+           (flights_featured["month"].isin([3, 4, 5])),
+           (flights_featured["month"].isin([6, 7, 8])),
+           (flights_featured["month"].isin([9, 10, 11]))]
+
+seasons = ['Winter', 'Spring', 'Summer', 'Fall']
+
+flights_featured['season'] = np.select(periods, seasons, default=None)
+
+# %%
+#checking featured dataset
+flights_featured.head()
+
+# =========== saving the final DF ===========
+flights_featured.to_csv(DATA_DIR / 'flights_featured.csv', index=False)
